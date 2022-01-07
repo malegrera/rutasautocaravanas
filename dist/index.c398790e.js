d14054b9d3efa2cd405bccebd23943c03751908c
Pilar Alegre Ramos <malegrera@uoc.edu>
2022-01-07 01:36:21 +0100

    Modificaciones PEC3

--- dist/index.c398790e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".fecha").textContent=(new Date).toLocaleDateString("es-ES",{weekday:"long",year:"numeric",month:"long",day:"numeric"}),document.querySelector(".fechac").textContent=(new Date).toLocaleDateString("es-ES",{year:"numeric",month:"numeric",day:"numeric"});
-//# sourceMappingURL=index.c398790e.js.map
