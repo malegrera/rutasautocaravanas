00a0d78f833879105b6aa4fb8e95d4611c5e36dd
malegrera <malegrera@uoc.edu>
2022-01-08 16:39:54 +0100

    PEC3

--- dist/index.5f2b248a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".fecha").textContent=(new Date).toLocaleDateString("es-ES",{weekday:"long",year:"numeric",month:"long",day:"numeric"}),document.querySelector(".fechac").textContent=(new Date).toLocaleDateString("es-ES",{year:"numeric",month:"numeric",day:"numeric"});
-//# sourceMappingURL=index.5f2b248a.js.map
